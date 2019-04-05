7f50f4997cd8577f6bd45dd8fc033116ee49bda0
Thomas Rory Gummerson <thomas@gummerson.no>
2019-04-05 11:13:34 +0200

    fix dumb output

--- shift.bat
+++ shift.bat
@@ -12,7 +12,7 @@
 @goto loop
 :break
 @set /A __shift_mi=%__shift_i%-1
-set "%~2_%__shift_mi%="
+@set "%~2_%__shift_mi%="
 @set /A "%~2_length=%__shift_length%-1"
 @set __shift_length=
 @set __shift_i=
